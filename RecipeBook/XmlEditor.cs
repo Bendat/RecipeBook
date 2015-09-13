776bc88a67cb97a26507bd940088fcafd9b78535
Bendat <benaherne@outlook.ie>
2015-09-13 18:03:01 +0100

    Revert "Small Things"
    This reverts commit 9fc5e25eaa31a588da8d15bb4b9aef2640221daa.

--- RecipeBook/XmlEditor.cs
+++ RecipeBook/XmlEditor.cs
@@ -60,8 +60,6 @@ namespace RecipeBook
             recipesNode.AppendChild(node);
             Doc.Save(Filename);
         }
-
-        
         /// <summary>
         /// Creates a new node with the data provided to the XmlEditor instance.
         /// </summary>
